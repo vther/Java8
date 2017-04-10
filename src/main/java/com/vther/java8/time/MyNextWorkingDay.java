41064d13304f4f775ba3da371c440f460dc9c92b
liuyunguo <vther@users.noreply.github.com>
2017-04-10 20:34:29 +0800

    提交java 8 dateAPI

--- src/main/java/com/vther/java8/time/MyNextWorkingDay.java
+++ src/main/java/com/vther/java8/time/MyNextWorkingDay.java
@@ -3,6 +3,7 @@ package com.vther.java8.time;
 import java.time.DayOfWeek;
 import java.time.Duration;
 import java.time.LocalDate;
+import java.time.Period;
 import java.time.temporal.*;
 
 
@@ -19,8 +20,8 @@ public class MyNextWorkingDay implements TemporalAdjuster {
             //获取了下个月的第一天
             Temporal temporal2 = LocalDate.now().with(TemporalAdjusters.firstDayOfNextMonth());
             // 获取了要减去
-            Temporal temporal3 = temporal.minus(temporal2.get(ChronoField.DAY_OF_WEEK) % 7 + 1, ChronoUnit.DAYS);
-            if (Duration.between(temporal3, temporal.plus(1L, ChronoUnit.DAYS)).isZero()) {
+            LocalDate temporal3 = (LocalDate) temporal.minus(temporal2.get(ChronoField.DAY_OF_WEEK) % 7 + 1, ChronoUnit.DAYS);
+            if (Period.between(temporal3,(LocalDate) temporal.plus(1L, ChronoUnit.DAYS)).getDays() == 0) {
                 dayToAdd = 1;
             } else {
                 dayToAdd = 3;
@@ -28,7 +29,6 @@ public class MyNextWorkingDay implements TemporalAdjuster {
 
         }
         if (dow == DayOfWeek.SATURDAY) dayToAdd = 2;
-
 
         return temporal.plus(dayToAdd, ChronoUnit.DAYS);
     }
